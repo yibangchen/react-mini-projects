23cd8174894f23b0fffb32559aa7c93ede736447
Yibang Chen <ychen622@gmail.com>
2019-07-01 00:25:18 -0700

    guessing game init

---
+++ color-memory-game/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+  const PAIR_NUM = 10;
+  const allColors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond",
+              "Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate",
+              "Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod",
+              "DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange",
+              "DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey",
+              "DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
+              "FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod",
+              "Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki",
+              "Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan",
+              "LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon",
+              "LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow",
+              "Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid",
+              "MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise",
+              "MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy",
+              "OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen",
+              "PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue",
+              "Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen",
+              "SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen",
+              "SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke",
+              "Yellow","YellowGreen"];
+
+class App extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      colors: [],
+      guessed: []
+    };
+  }
+
+  initGame = () => { 
+    // pick # colors, fill random 2 blocks in colors; init guessed to empty false
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+  
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
---
+++ color-memory-game/src/Block.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Block.css';
+
+const Block = (props) => {
+	static defaultProps = {
+		onClickBlock() {}
+	}
+
+	static propTypes = {
+		onClickBlock: PropTypes.func
+	}
+
+	return (
+		<div className="block" onClick={props.onClickBlock}></div>
+	);
+}
+
+export default Block;

2691692cbc8f06238854042cca61a73c7f4245c9
Yibang Chen <ychen622@gmail.com>
2019-07-05 17:57:57 -0700

    success section as footer

--- memory-game-colors/src/App.js
+++ memory-game-colors/src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import GameBoard from './GameBoard';
+import Success from './Success';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <GameBoard />
-  </div>
-);
+const STATUS = {
+  INPROGRESS: 0,
+  WIN: 1,
+  LOST: 2
+} 
 
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      winStatus: STATUS.INPROGRESS
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <GameBoard />
+        <Success status={ this.state.winStatus } />
+      </div>
+    );
+  }
+}
 export default App;
---
+++ memory-game-colors/src/Success.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Success.css';
+
+const Success = (props) => (
+	<div className="Success">
+		<div>
+			<h1>GREAT JOB!</h1>
+			<button>restart</button>
+			<button>Harder</button>
+		</div>
+	</div>
+);
+
+export default Success;

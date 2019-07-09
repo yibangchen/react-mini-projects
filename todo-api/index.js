00bfe3ee4c2001feaf6d3ce84396b08d59d99c17
Yibang Chen <ychen622@gmail.com>
2019-07-09 15:17:03 -0700

    api complete

--- todo-api/index.js
+++ todo-api/index.js
@@ -1,8 +1,12 @@
 var express = require('express'),
 	app = express(),
-	port = process.env.PORT || 3000;
+	port = process.env.PORT || 3000,
+	bodyParser = require('body-parser');
 
 var todoRoutes = require('./routes/todo');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res)=> {
 	res.send("Hi there from express");
--- todo-api/routes/todo.js
+++ todo-api/routes/todo.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var db = require("../models");
+var helpers = require('../helpers/todos');
 
-router.get('/', (req, res) => {
-	db.Todo.find()
-	.then( todos => {
-		res.json(todos);
-	})
-	.catch( err => {
-		res.send(err);
-	})
-});
+router.route('/')
+	.get(helpers.getTodos)
+	.post(helpers.createTodo);
+
+router.route('/:todoId')
+	.get(helpers.getTodo)
+	.put(helpers.updateTodo)
+	.delete(helpers.deleteTodo);
 
 module.exports = router;

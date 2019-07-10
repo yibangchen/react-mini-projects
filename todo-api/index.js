0cea4cc6e90376e2d4c773941dd45e93fff75789
Yibang Chen <ychen622@gmail.com>
2019-07-09 20:38:45 -0700

    single page app

--- todo-api/index.js
+++ todo-api/index.js
@@ -7,6 +7,12 @@ var todoRoutes = require('./routes/todo');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', (req, res) => {
+	res.sendFile('index.html');
+})
 
 app.get('/', (req, res)=> {
 	res.send("Hi there from express");
---
+++ todo-api/public/app.js
@@ -0,0 +1,75 @@
+
+$(document).ready(function(){
+  $.getJSON("/api/todos")
+  .then(addTodos)
+
+  $('#todoInput').keypress( e => {
+  	if (e.which === 13) createTodo();
+  });
+
+  $('.list').on('click', 'li', function(){
+    updateTodo($(this));
+  })
+  
+  $('.list').on('click', 'span', function(e){
+    e.stopPropagation();
+    removeTodo($(this).parent());
+  })
+});
+
+function addTodos(todos) {
+  //add todos to page here
+  todos.forEach(addTodo);
+}
+
+function addTodo(todo){
+	var newTodo = $(`<li class="task">${todo.name} <span>X</span></li>`);
+	newTodo.data('id', todo._id);
+	newTodo.data('completed', todo.completed);
+	if (todo.completed)
+		newTodo.addClass("done");
+	$('.list').append(newTodo);
+}
+
+function createTodo(){
+  //send request to create new todo
+  var usrInput = $('#todoInput').val();
+  $.post('/api/todos', { name: usrInput })
+  .then(newTodo => {
+  	$('#todoInput').val("");
+  	addTodo(newTodo);
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+}
+
+function removeTodo(todo){
+  var clickedId = todo.data('id');
+  var deleteUrl = '/api/todos/' + clickedId; 
+  $.ajax({
+  	method: 'DELETE',
+  	url: deleteUrl
+  })
+  .then(data => {
+    todo.remove();
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
+
+function updateTodo(todo){
+  var updateUrl = '/api/todos/' + todo.data('id');
+  var isDone = !todo.data('completed');
+  var updateData = {completed: isDone}
+  $.ajax({
+    method: 'PUT',
+    url: updateUrl,
+    data: updateData
+  })
+  .then(function(updatedTodo){
+    todo.toggleClass("done");
+    todo.data('completed', isDone);
+  })
+}
